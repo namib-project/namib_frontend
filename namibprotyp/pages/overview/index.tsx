48f1fbcb5641d4117305818a044929480c2130f3
Fabian Riedemann <fari@uni-bremen.de>
2020-11-10 12:34:42 +0100

    Sass integration

--- namibprotyp/pages/overview/index.tsx
+++ namibprotyp/pages/overview/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Drawer from "../drawer";
 
-import style from "./overview.module.css";
+import style from "./overview.module.scss";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles({
