dbe7856daffa78f8b6e72c7f2cf5fa5190a29acb
kweller <kweller@uni-bremen.de>
2020-11-11 21:20:25 +0100

    networkoverview added

--- namibprotyp/pages/drawer.tsx
+++ namibprotyp/pages/drawer.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import clsx from "clsx";
 import {
-    makeStyles,
-    useTheme,
-    Theme,
-    createStyles,
+  makeStyles,
+  useTheme,
+  Theme,
+  createStyles,
 } from "@material-ui/core/styles";
 import Drawer from "@material-ui/core/Drawer";
 import CssBaseline from "@material-ui/core/CssBaseline";
@@ -22,233 +22,228 @@ import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
 import InboxIcon from "@material-ui/icons/MoveToInbox";
 import MailIcon from "@material-ui/icons/Mail";
-import {withRouter} from "react-router-dom";
+import { withRouter } from "react-router-dom";
 import Link from "next/link";
-import style from './overview/overview.module.scss'
-import {Button} from "@material-ui/core";
-import {Add} from "@material-ui/icons";
-import SettingsIcon from '@material-ui/icons/Settings';
+import style from "./overview/overview.module.scss";
+import { Button } from "@material-ui/core";
+import { Add } from "@material-ui/icons";
+import SettingsIcon from "@material-ui/icons/Settings";
+import ShowChartIcon from "@material-ui/icons/ShowChart";
 
 const drawerWidth = 240;
 
 const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
-        root: {
-            display: "flex",
-        },
-        appBar: {
-            transition: theme.transitions.create(["margin", "width"], {
-                easing: theme.transitions.easing.sharp,
-                duration: theme.transitions.duration.leavingScreen,
-            }),
-        },
-        appBarShift: {
-            width: `calc(100% - ${drawerWidth}px)`,
-            marginLeft: drawerWidth,
-            transition: theme.transitions.create(["margin", "width"], {
-                easing: theme.transitions.easing.easeOut,
-                duration: theme.transitions.duration.enteringScreen,
-            }),
-        },
-        menuButton: {
-            marginRight: theme.spacing(2),
-        },
-        hide: {
-            display: "none",
-        },
-        drawer: {
-            width: drawerWidth,
-            flexShrink: 0,
-        },
-        drawerPaper: {
-            width: drawerWidth,
-        },
-        drawerHeader: {
-            display: "flex",
-            alignItems: "center",
-            padding: theme.spacing(0, 1),
-            // necessary for content to be below app bar
-            ...theme.mixins.toolbar,
-            justifyContent: "flex-end",
-        },
-        content: {
-            flexGrow: 1,
-            padding: theme.spacing(3),
-            transition: theme.transitions.create("margin", {
-                easing: theme.transitions.easing.sharp,
-                duration: theme.transitions.duration.leavingScreen,
-            }),
-            marginLeft: -drawerWidth,
-        },
-        contentShift: {
-            transition: theme.transitions.create("margin", {
-                easing: theme.transitions.easing.easeOut,
-                duration: theme.transitions.duration.enteringScreen,
-            }),
-            marginLeft: 0,
-        },
-    })
+  createStyles({
+    root: {
+      display: "flex",
+    },
+    appBar: {
+      transition: theme.transitions.create(["margin", "width"], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+    },
+    appBarShift: {
+      width: `calc(100% - ${drawerWidth}px)`,
+      marginLeft: drawerWidth,
+      transition: theme.transitions.create(["margin", "width"], {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    hide: {
+      display: "none",
+    },
+    drawer: {
+      width: drawerWidth,
+      flexShrink: 0,
+    },
+    drawerPaper: {
+      width: drawerWidth,
+    },
+    drawerHeader: {
+      display: "flex",
+      alignItems: "center",
+      padding: theme.spacing(0, 1),
+      // necessary for content to be below app bar
+      ...theme.mixins.toolbar,
+      justifyContent: "flex-end",
+    },
+    content: {
+      flexGrow: 1,
+      padding: theme.spacing(3),
+      transition: theme.transitions.create("margin", {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+      marginLeft: -drawerWidth,
+    },
+    contentShift: {
+      transition: theme.transitions.create("margin", {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      marginLeft: 0,
+    },
+  })
 );
 
 export default function PersistentDrawerLeft(props) {
-    const classes = useStyles();
-    const theme = useTheme();
-    const [open, setOpen] = React.useState(false);
+  const classes = useStyles();
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
 
-    const itemsListBasic = [
-        {
-            text: "Geräteübersicht",
-            icon: <InboxIcon/>,
-            path: "/deviceOverview",
-        },
-        {
-            text: "BasisStuff",
-            icon: <MailIcon/>,
-            path: "/overview",
-        },
-    ];
+  const itemsListBasic = [
+    {
+      text: "Geräteübersicht",
+      icon: <InboxIcon />,
+      path: "/deviceOverview",
+    },
+    {
+      text: "BasisStuff",
+      icon: <MailIcon />,
+      path: "/overview",
+    },
+  ];
 
-    const itemsListAdvanced = [
-        {
-            text: "Netzwerkverhalten",
-            icon: <InboxIcon/>,
-            path: "/networkbehaviour",
-        },
-        {
-            text: "MUD Profil anlegen",
-            icon: <Add/>,
-            path: "/createMudProfile",
-        },
-        {
-            text: "MUD Profil auswählen",
-            icon: <InboxIcon/>,
-            path: "/networkbehaviour",
-        },
-    ];
+  const itemsListAdvanced = [
+    {
+      text: "Netzwerkverhalten",
+      icon: <ShowChartIcon />,
+      path: "/networkbehaviour",
+    },
+    {
+      text: "MUD Profil anlegen",
+      icon: <Add />,
+      path: "/createMudProfile",
+    },
+    {
+      text: "MUD Profil auswählen",
+      icon: <InboxIcon />,
+      path: "/networkbehaviour",
+    },
+  ];
 
-    const ItemListSettingsButSeperated = [
-        {
-            text: "Settings",
-            icon: <SettingsIcon/>,
-            path: "/settings",
-        },
-    ];
+  const ItemListSettingsButSeperated = [
+    {
+      text: "Settings",
+      icon: <SettingsIcon />,
+      path: "/settings",
+    },
+  ];
 
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
 
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
 
-
-    const handleDrawerOpen = () => {
-        setOpen(true);
-    };
-
-    const handleDrawerClose = () => {
-        setOpen(false);
-    };
-
-    return (
-        <div className={classes.root}>
-            <CssBaseline/>
-            <AppBar
-                position="fixed"
-                className={clsx(classes.appBar, {
-                    [classes.appBarShift]: open,
-                })}
-            >
-                <Toolbar>
-                    <IconButton
-                        color="inherit"
-                        aria-label="open drawer"
-                        onClick={handleDrawerOpen}
-                        edge="start"
-                        className={clsx(classes.menuButton, open && classes.hide)}
-                    >
-                        <MenuIcon/>
-                    </IconButton>
-                    <Typography variant="h6" noWrap>
-                        NAMIB
-                    </Typography>
-                    <div>
-                        <Link href={"/"}>
-                            <a className={style.logoutLink}>
-                                <Button className={style.logoutButton} variant="contained">Logout</Button>
-                            </a>
-                        </Link>
-                    </div>
-                </Toolbar>
-            </AppBar>
-            <Drawer
-                className={classes.drawer}
-                variant="persistent"
-                anchor="left"
-                open={open}
-                classes={{
-                    paper: classes.drawerPaper,
-                }}
-            >
-                <div className={classes.drawerHeader}>
-                    <IconButton onClick={handleDrawerClose}>
-                        {theme.direction === "ltr" ? (
-                            <ChevronLeftIcon/>
-                        ) : (
-                            <ChevronRightIcon/>
-                        )}
-                    </IconButton>
-                </div>
-                <Divider/>
-                <List>
-                    {itemsListBasic.map((item, index) => {
-                        const {text, icon, path} = item;
-                        return (
-                            <Link href={path}>
-                                <ListItem button key={text}>
-                                    {icon && <ListItemIcon>{icon}</ListItemIcon>}
-                                    <ListItemText primary={text}/>
-                                </ListItem>
-                            </Link>
-                        );
-                    })}
-                </List>
-                <Divider/>
-                <List>
-                    {itemsListAdvanced.map((item, index) => {
-                        const {text, icon, path} = item;
-                        return (
-                            <Link href={path}>
-                                <ListItem button key={text}>
-                                    {icon && <ListItemIcon>{icon}</ListItemIcon>}
-                                    <ListItemText primary={text}/>
-                                </ListItem>
-                            </Link>
-                        );
-                    })}
-                </List>
-                <Divider/>
-                <List>
-                    {ItemListSettingsButSeperated.map((item, index) => {
-                        const {text, icon, path} = item;
-                        return (
-                            <Link href={path}>
-                                <ListItem button key={text}>
-                                    {icon && <ListItemIcon>{icon}</ListItemIcon>}
-                                    <ListItemText primary={text}/>
-                                </ListItem>
-                            </Link>
-                        );
-                    })}
-                </List>
-
-
-
-
-
-            </Drawer>
-            <main
-                className={clsx(classes.content, {
-                    [classes.contentShift]: open,
-                })}
-            >
-                <div className={classes.drawerHeader}/>
-            </main>
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        className={clsx(classes.appBar, {
+          [classes.appBarShift]: open,
+        })}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            className={clsx(classes.menuButton, open && classes.hide)}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap>
+            NAMIB
+          </Typography>
+          <div>
+            <Link href={"/"}>
+              <a className={style.logoutLink}>
+                <Button className={style.logoutButton} variant="contained">
+                  Logout
+                </Button>
+              </a>
+            </Link>
+          </div>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        className={classes.drawer}
+        variant="persistent"
+        anchor="left"
+        open={open}
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <div className={classes.drawerHeader}>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === "ltr" ? (
+              <ChevronLeftIcon />
+            ) : (
+              <ChevronRightIcon />
+            )}
+          </IconButton>
         </div>
-    );
+        <Divider />
+        <List>
+          {itemsListBasic.map((item, index) => {
+            const { text, icon, path } = item;
+            return (
+              <Link href={path}>
+                <ListItem button key={text}>
+                  {icon && <ListItemIcon>{icon}</ListItemIcon>}
+                  <ListItemText primary={text} />
+                </ListItem>
+              </Link>
+            );
+          })}
+        </List>
+        <Divider />
+        <List>
+          {itemsListAdvanced.map((item, index) => {
+            const { text, icon, path } = item;
+            return (
+              <Link href={path}>
+                <ListItem button key={text}>
+                  {icon && <ListItemIcon>{icon}</ListItemIcon>}
+                  <ListItemText primary={text} />
+                </ListItem>
+              </Link>
+            );
+          })}
+        </List>
+        <Divider />
+        <List>
+          {ItemListSettingsButSeperated.map((item, index) => {
+            const { text, icon, path } = item;
+            return (
+              <Link href={path}>
+                <ListItem button key={text}>
+                  {icon && <ListItemIcon>{icon}</ListItemIcon>}
+                  <ListItemText primary={text} />
+                </ListItem>
+              </Link>
+            );
+          })}
+        </List>
+      </Drawer>
+      <main
+        className={clsx(classes.content, {
+          [classes.contentShift]: open,
+        })}
+      >
+        <div className={classes.drawerHeader} />
+      </main>
+    </div>
+  );
 }
