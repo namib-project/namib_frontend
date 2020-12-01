3a23ba8905d940bc451458b94a237dff43cc58c9
kweller <kweller@uni-bremen.de>
2020-12-01 21:16:24 +0100

    expert mode third example

--- flutter_protyp/lib/pages/settings.dart
+++ flutter_protyp/lib/pages/settings.dart
@@ -25,107 +25,133 @@ class _SettingsState extends State<Settings> {
       body: Center(
         child: Container(
           width: 300,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                'settings'.tr().toString(),
-                style: TextStyle(
-                  fontFamily: "OpenSans",
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'settings'.tr().toString(),
+                  style: TextStyle(
+                    fontFamily: "OpenSans",
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              Container(
-                alignment: Alignment.centerLeft,
-                height: 60,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      "Dark mode:",
+                ExpansionTile(
+                  title: Text("Beschreibung",
                       style: TextStyle(
-                        fontSize: 22,
-                      ),
+                        color: primaryColor,
+                        fontSize: 18,
+                      )),
+                  children: <Widget>[
+                    Text(
+                      "Dark mode: Wählen aus für Dark mode",
+                      style: TextStyle(fontSize: 18),
                     ),
-                    Switch(
-                      value: darkMode,
-                      activeColor: buttonColor,
-                      onChanged: (bool s) {
-                        setState(() {
-                          darkMode
-                              ? themeChanger.setTheme(ThemeData.light())
-                              : themeChanger.setTheme(ThemeData.dark());
-                          darkMode = !darkMode;
-                        });
-                      },
+                    Text(
+                      "Expertenmodus: Wählen aus für Expertenmodus",
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    Text(
+                      "Sprache: Wählen Sie die gewünschte Sprache",
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    SizedBox(
+                      height: 10,
                     ),
                   ],
                 ),
-              ),
-              Container(
-                alignment: Alignment.centerLeft,
-                height: 60,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      'expertMode'.tr().toString(),
-                      style: TextStyle(
-                        fontSize: 22,
-                      ),
-                    ),
-                    Switch(
-                      value: expertMode,
-                      activeColor: buttonColor,
-                      onChanged: (bool s) {
-                        setState(() {
-                          expertMode = s;
-                        });
-                      },
-                    )
-                  ],
+                SizedBox(
+                  height: 10,
                 ),
-              ),
-              Container(
-                alignment: Alignment.centerLeft,
-                height: 60,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      'language'.tr().toString(),
-                      style: TextStyle(
-                        fontSize: 22,
+                Container(
+                  alignment: Alignment.centerLeft,
+                  height: 60,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Dark mode",
+                        style: TextStyle(
+                          fontSize: 22,
+                        ),
                       ),
-                    ),
-                    FlatButton(
-                      onPressed: () {
-                        // set german language
-                        setState(() {
-                          EasyLocalization.of(context).locale =
-                              Locale('de', 'DE');
-                        });
-                      },
-                      child: Text('german'.tr().toString()),
-                    ),
-                    FlatButton(
-                      onPressed: () {
-                        // set english language
-                        setState(() {
-                          EasyLocalization.of(context).locale =
-                              Locale('en', 'US');
-                        });
-                      },
-                      child: Text('english'.tr().toString()),
-                    ),
-                  ],
+                      Switch(
+                        value: darkMode,
+                        activeColor: buttonColor,
+                        onChanged: (bool s) {
+                          setState(() {
+                            darkMode
+                                ? themeChanger.setTheme(ThemeData.light())
+                                : themeChanger.setTheme(ThemeData.dark());
+                            darkMode = !darkMode;
+                          });
+                        },
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            ],
+                Container(
+                  alignment: Alignment.centerLeft,
+                  height: 60,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'expertMode'.tr().toString(),
+                        style: TextStyle(
+                          fontSize: 22,
+                        ),
+                      ),
+                      Switch(
+                        value: expertMode,
+                        activeColor: buttonColor,
+                        onChanged: (bool s) {
+                          setState(() {
+                            expertMode = s;
+                          });
+                        },
+                      )
+                    ],
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.centerLeft,
+                  height: 60,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'language'.tr().toString(),
+                        style: TextStyle(
+                          fontSize: 22,
+                        ),
+                      ),
+                      FlatButton(
+                        onPressed: () {
+                          // set german language
+                          setState(() {
+                            EasyLocalization.of(context).locale =
+                                Locale('de', 'DE');
+                          });
+                        },
+                        child: Text('german'.tr().toString()),
+                      ),
+                      FlatButton(
+                        onPressed: () {
+                          // set english language
+                          setState(() {
+                            EasyLocalization.of(context).locale =
+                                Locale('en', 'US');
+                          });
+                        },
+                        child: Text('english'.tr().toString()),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
@@ -134,7 +160,7 @@ class _SettingsState extends State<Settings> {
 }
 
 /**
-   FlatButton(
+    FlatButton(
     child: Text('Dark Theme'),
     onPressed: () => _themeChanger.setTheme(ThemeData.dark())),
     FlatButton(
