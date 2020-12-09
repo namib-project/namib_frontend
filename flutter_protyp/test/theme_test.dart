2719275c87e859c37726c125be2ad4bbbf6a3c0b
kweller <kweller@uni-bremen.de>
2020-12-09 23:18:30 +0100

    small change in tests

--- flutter_protyp/test/theme_test.dart
+++ flutter_protyp/test/theme_test.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_protyp/widgets/theme.dart';
-import 'package:provider/provider.dart';
 
 void themeTest() {
+  /// tests the setTheme function in theme.dart
   test("Test with all possible values for setTheme", () {
-    ThemeChanger(ThemeData.light());
-
     ThemeChanger themeChanger = new ThemeChanger(ThemeData.light());
     themeChanger.setTheme(ThemeData.dark());
     expect(themeChanger.getTheme(), ThemeData.dark());
