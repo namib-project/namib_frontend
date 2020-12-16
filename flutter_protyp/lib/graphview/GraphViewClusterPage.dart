deba3ddc2fb7e099b23da1832d9872ce08b6faf0
Jan Lüeße <janlue@uni-bremen.de>
2020-12-16 14:49:05 +0100

    required files for graphs

---
+++ flutter_protyp/lib/graphview/GraphViewClusterPage.dart
@@ -0,0 +1,104 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_protyp/widgets/appbar.dart';
+import 'package:flutter_protyp/widgets/drawer.dart';
+import 'package:graphview/GraphView.dart';
+
+class GraphClusterViewPage extends StatefulWidget {
+  @override
+  _GraphClusterViewPageState createState() => _GraphClusterViewPageState();
+}
+
+class _GraphClusterViewPageState extends State<GraphClusterViewPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: MainAppbar(),
+        drawer: MainDrawer(),
+        body: Column(
+          children: [
+            InteractiveViewer(
+                constrained: true,
+                boundaryMargin: EdgeInsets.all(8),
+                minScale: 0.001,
+                maxScale: 100,
+                child: GraphView(
+                  graph: graph,
+                  algorithm: builder,
+                  paint: Paint()
+                    ..color = Colors.green
+                    ..strokeWidth = 1
+                    ..style = PaintingStyle.fill,
+                )),
+          ],
+        ));
+  }
+
+  int n = 8;
+  Random r = Random();
+
+  Widget getNodeText(int i) {
+    return GestureDetector(
+      onLongPressStart: (details) {
+        var x = details.globalPosition.dx;
+        var y = details.globalPosition.dy;
+        Offset(x, y);
+      },
+      onPanStart: (details) {
+        var x = details.globalPosition.dx;
+        var y = details.globalPosition.dy;
+        setState(() {
+          builder.setFocusedNode(graph.getNodeAtPosition(i - 1));
+          graph.getNodeAtPosition(i - 1).position = Offset(x, y);
+        });
+      },
+      onPanUpdate: (details) {
+        var x = details.globalPosition.dx;
+        var y = details.globalPosition.dy;
+        setState(() {
+          builder.setFocusedNode(graph.getNodeAtPosition(i - 1));
+          graph.getNodeAtPosition(i - 1).position = Offset(x, y);
+        });
+      },
+      onPanEnd: (details) {
+        builder.setFocusedNode(null);
+      },
+      child: Container(
+          padding: EdgeInsets.all(16),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(4),
+            boxShadow: [
+              BoxShadow(color: Colors.blue[100], spreadRadius: 1),
+            ],
+          ),
+          child:  Icon(Icons.device_unknown)) //("Node $i")),
+    );
+  }
+
+  final Graph graph = Graph();
+  Layout builder;
+
+  @override
+  void initState() {
+    final a = Node(getNodeText(1));
+    final b = Node(getNodeText(2));
+    final c = Node(getNodeText(3));
+    final d = Node(getNodeText(4));
+    final e = Node(getNodeText(5));
+    final f = Node(getNodeText(6));
+    final g = Node(getNodeText(7));
+    final h = Node(getNodeText(8));
+
+    graph.addEdge(a, b, paint: Paint()..color = Colors.red);
+    graph.addEdge(a, c);
+    graph.addEdge(a, d);
+    graph.addEdge(c, e);
+    graph.addEdge(d, f);
+    graph.addEdge(f, c);
+    graph.addEdge(g, c);
+    graph.addEdge(h, g);
+
+    builder = FruchtermanReingoldAlgorithm(iterations: 1000);
+  }
+}
---
+++ flutter_protyp/lib/graphview/TreeViewPage.dart
@@ -0,0 +1,154 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_protyp/widgets/appbar.dart';
+import 'package:flutter_protyp/widgets/drawer.dart';
+import 'package:graphview/GraphView.dart';
+
+class TreeViewPage extends StatefulWidget {
+  @override
+  _TreeViewPageState createState() => _TreeViewPageState();
+}
+
+class _TreeViewPageState extends State<TreeViewPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: MainAppbar(),
+        drawer: MainDrawer(),
+        body: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Wrap(
+              children: [
+                Container(
+                  width: 100,
+                  child: TextFormField(
+                    initialValue: builder.siblingSeparation.toString(),
+                    decoration: InputDecoration(labelText: "Sibling Separation"),
+                    onChanged: (text) {
+                      builder.siblingSeparation = int.tryParse(text) ?? 100;
+                      this.setState(() {});
+                    },
+                  ),
+                ),
+                Container(
+                  width: 100,
+                  child: TextFormField(
+                    initialValue: builder.levelSeparation.toString(),
+                    decoration: InputDecoration(labelText: "Level Separation"),
+                    onChanged: (text) {
+                      builder.levelSeparation = int.tryParse(text) ?? 100;
+                      this.setState(() {});
+                    },
+                  ),
+                ),
+                Container(
+                  width: 100,
+                  child: TextFormField(
+                    initialValue: builder.subtreeSeparation.toString(),
+                    decoration: InputDecoration(labelText: "Subtree separation"),
+                    onChanged: (text) {
+                      builder.subtreeSeparation = int.tryParse(text) ?? 100;
+                      this.setState(() {});
+                    },
+                  ),
+                ),
+                Container(
+                  width: 100,
+                  child: TextFormField(
+                    initialValue: builder.orientation.toString(),
+                    decoration: InputDecoration(labelText: "Orientation"),
+                    onChanged: (text) {
+                      builder.orientation = int.tryParse(text) ?? 100;
+                      this.setState(() {});
+                    },
+                  ),
+                ),
+                RaisedButton(
+                  onPressed: () {
+                    final Node node12 = Node(getNodeText());
+                    var edge = graph.getNodeAtPosition(r.nextInt(graph.nodeCount()));
+                    print(edge);
+                    graph.addEdge(edge, node12);
+                    setState(() {});
+                  },
+                  child: Text("Add"),
+                )
+              ],
+            ),
+            InteractiveViewer(
+                constrained: true,
+                boundaryMargin: EdgeInsets.all(100),
+                minScale: 0.01,
+                maxScale: 5.6,
+                child: GraphView(
+                  graph: graph,
+                  algorithm: BuchheimWalkerAlgorithm(builder, TreeEdgeRenderer(builder)),
+                  paint: Paint()
+                    ..color = Colors.green
+                    ..strokeWidth = 1
+                    ..style = PaintingStyle.stroke,
+                )),
+          ],
+        ));
+  }
+
+  Random r = Random();
+
+  int n = 1;
+
+  Widget getNodeText() {
+    return InkWell(
+      onTap: () {
+        print('clicked');
+      },
+      child: Container(
+          padding: EdgeInsets.all(16),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(4),
+            boxShadow: [
+              BoxShadow(color: Colors.blue[100], spreadRadius: 1),
+            ],
+          ),
+          child: Text("Node ${n++}")),
+    );
+  }
+
+  final Graph graph = Graph();
+  BuchheimWalkerConfiguration builder = BuchheimWalkerConfiguration();
+
+  @override
+  void initState() {
+    final Node node1 = Node(getNodeText());
+    final Node node2 = Node(getNodeText());
+    final Node node3 = Node(getNodeText());
+    final Node node4 = Node(getNodeText());
+    final Node node5 = Node(getNodeText());
+    final Node node6 = Node(getNodeText());
+    final Node node8 = Node(getNodeText());
+    final Node node7 = Node(getNodeText());
+    final Node node9 = Node(getNodeText());
+    final Node node10 = Node(getNodeText());
+    final Node node11 = Node(getNodeText());
+    final Node node12 = Node(getNodeText());
+
+    graph.addEdge(node1, node2, paint: Paint()..color = Colors.red);
+    graph.addEdge(node1, node3);
+    graph.addEdge(node1, node4);
+    graph.addEdge(node2, node5);
+    graph.addEdge(node2, node6);
+    graph.addEdge(node6, node7);
+    graph.addEdge(node6, node8);
+    graph.addEdge(node4, node9);
+    graph.addEdge(node4, node10);
+    graph.addEdge(node4, node11);
+    graph.addEdge(node11, node12);
+
+    builder
+      ..siblingSeparation = (100)
+      ..levelSeparation = (150)
+      ..subtreeSeparation = (150)
+      ..orientation = (BuchheimWalkerConfiguration.ORIENTATION_TOP_BOTTOM);
+  }
+}
