9b3b021f85c783e42d04b4c191475c298fb6e76f
Fabian Riedemann <fari@uni-bremen.de>
2021-01-06 15:46:54 +0100

    wired addition

--- flutter_protyp/lib/data/device.g.dart
+++ flutter_protyp/lib/data/device.g.dart
@@ -8,12 +8,32 @@ part of 'device.dart';
 
 Device _$DeviceFromJson(Map<String, dynamic> json) {
   return Device(
-    deviceName: json['deviceName'] as String,
-    mudName: json['mudName'] as String,
+    systeminfo: json['systeminfo'] as String,
+    name: json['name'] as String,
+    software_rev: json['software_rev'] as String,
+    ipv4_addr: json['ipv4_addr'] as String,
+    image: json['image'] as String,
+    mud_url: json['mud_url'] as String,
+    mud_signature: json['mud_signature'] as String,
+    documentation: json['documentation'] as String,
+    services: (json['services'] as List)
+        ?.map((e) =>
+            e == null ? null : MUDService.fromJson(e as Map<String, dynamic>))
+        ?.toList(),
+    allowedDNSRequests:
+        (json['allowedDNSRequests'] as List)?.map((e) => e as String)?.toList(),
   );
 }
 
 Map<String, dynamic> _$DeviceToJson(Device instance) => <String, dynamic>{
-      'deviceName': instance.deviceName,
-      'mudName': instance.mudName,
+      'systeminfo': instance.systeminfo,
+      'name': instance.name,
+      'software_rev': instance.software_rev,
+      'ipv4_addr': instance.ipv4_addr,
+      'image': instance.image,
+      'mud_url': instance.mud_url,
+      'mud_signature': instance.mud_signature,
+      'documentation': instance.documentation,
+      'services': instance.services?.map((e) => e?.toJson())?.toList(),
+      'allowedDNSRequests': instance.allowedDNSRequests,
     };
