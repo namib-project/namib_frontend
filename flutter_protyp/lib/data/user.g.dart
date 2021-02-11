580400ea7a13943d9f1ed4282b02ffaca138ea46
Fabian Riedemann <fari@uni-bremen.de>
2021-02-11 15:04:36 +0100

    user.g.dart

--- flutter_protyp/lib/data/user.g.dart
+++ flutter_protyp/lib/data/user.g.dart
@@ -1,16 +1,22 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
-// This class is generated, it converts an user-model to map and other direction
+
 part of 'user.dart';
+// This class is generated, it converts an user-model to map and other direction
 
 // **************************************************************************
 // JsonSerializableGenerator
 // **************************************************************************
 
 User _$UserFromJson(Map<String, dynamic> json) {
-  return User(json['username'] as String, json['admin'] as bool);
+  return User(
+    json['username'] as String,
+    json['admin'] as bool,
+    json['user'] as bool,
+  );
 }
 
 Map<String, dynamic> _$UserToJson(User instance) => <String, dynamic>{
       'username': instance.username,
       'admin': instance.admin,
+      'user': instance.user,
     };
