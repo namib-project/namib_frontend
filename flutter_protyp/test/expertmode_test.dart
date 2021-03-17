9238e5196e69defb7063bf145cbcf8428149e494
Fabian Riedemann <fari@uni-bremen.de>
2021-03-17 13:58:28 +0100

    pipeline läuft hoffentlich weider

--- flutter_protyp/test/expertmode_test.dart
+++ flutter_protyp/test/expertmode_test.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_protyp/pages/settings.dart';
 import 'package:flutter_protyp/widgets/constant.dart';
 
 
 void themeTest() {
   /// tests the setTheme function in theme.dart
   test("Test with all possible values for set Expertmode", () {
-    Settings s = new Settings();
     Constant c = new Constant();
 
 
