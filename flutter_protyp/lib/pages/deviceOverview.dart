0703e8ef76b0a67ab72e91c191fe3f10b4666924
Fabian Riedemann <fari@uni-bremen.de>
2021-03-20 12:37:50 +0100

    deviceOverview cosmetics

--- flutter_protyp/lib/pages/deviceOverview.dart
+++ flutter_protyp/lib/pages/deviceOverview.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-import 'dart:math';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_protyp/data/device_mud/device.dart';
@@ -143,7 +142,6 @@ class _DeviceOverviewState extends State<DeviceOverview> {
     } else {
       throw Exception("Failed to get Data");
     }
-    //TODO bei release auf http request umstellen
   }
 
   dynamic _handleTimeOut() {}
